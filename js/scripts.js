1dd24054a64f1d882ee71c6eb31ffce88561a92f
Michael Summers <student@epicodus.com>
2015-09-11 11:01:24 -0700

    added in code to deal with problem-words/characters, and it morphed into a fits-all solution. One commit for 3 specs ggwp

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,18 @@
 var findReplace = function(original, target, replacement) {
   var modified = original.replace(target, replacement);
+  var testCaps = original.toLowerCase().replace(target.toLowerCase(),replacement.toLowerCase());
 
+  if (modified.toLowerCase() != testCaps) {
+    var arrModified = modified.split(" ");
+    var arrTestCaps = testCaps.split(" ");
+    for (var count = 0; count < arrModified.length; count++) {
+      if (arrModified[count].toLowerCase().replace(/[^a-z\n\t ,]/ig, "") != arrTestCaps[count].toLowerCase()) {
+        arrModified[count] = replacement;
+      }
+    }
+    modified = arrModified.join(" ");
+    return modified;
+    //return "Not equal (!=). If statement success. |**|".concat(modified).concat("|**|").concat(testCaps);
+  }
   return modified;
 };
--- spec/specs.js
+++ spec/specs.js
@@ -1,13 +1,28 @@
-describe('findReplace', function() {
+describe('findReplace', function () {
   it("stores 'original' string, 'target' word to change, and 'replacement' word, and returns replacement", function () {
     //I started off with Dethklok on my playlist and realized "Dethklok again?" and switched to pendulum.
     //Prelude
-    expect(findReplace("But tonight, on this small planet, on Mars, we're going to rock civilization","Mars" , "Earth")).to.equal("But tonight, on this small planet, on Earth, we're going to rock civilization")
+    expect(findReplace("But tonight, on this small planet, on Mars, we're going to rock civilization","Mars" , "Earth")).to.equal("But tonight, on this small planet, on Earth, we're going to rock civilization");
   });
 
   //Through the Loop - Willie Wonka dark-tunnel scene set to drum and base as intro. Not quite my favorite Pendulum song, but its close...
-  it("stores original string, target, and replacement, and returns a modified string", function (){
-    expect(findReplace("YES! The danger must be shrinking, for the rowers keep on rowing, and they're certainly not showing any signs that they're slowing","shrinking","growing")).to.equal("YES! The danger must be growing, for the rowers keep on rowing, and they're certainly not showing any signs that they're slowing")
+  it("stores original string, target, and replacement, and returns a modified string", function () {
+    expect(findReplace("YES! The danger must be shrinking, for the rowers keep on rowing, and they're certainly not showing any signs that they're slowing","shrinking","growing")).to.equal("YES! The danger must be growing, for the rowers keep on rowing, and they're certainly not showing any signs that they're slowing");
+  });
+
+  //Tarantula
+  it("stores original/target/replacement, but tests capitals/lowercase", function () {
+    expect(findReplace("Through the eye of the storm, With a GOLDEN lining, To the point where it breaks, Gonna keep on grinding", "golden", "silver")).to.equal("Through the eye of the storm, With a silver lining, To the point where it breaks, Gonna keep on grinding");
+  });
+
+  //Under the Waves
+  it("Tests a crazy-capitals case and replaces it with a different but regular looking word", function () {
+    expect(findReplace("I am deep in a OcEaN, Waves above my head, Help with no spirit", "OCEAN", "river"));
+  });
+
+  // No theme for this one. . .
+  it("tests one last case with strange or mistyped non comma/space/letters", function () {
+    expect(findReplace("Ap!pl*e computers", "apple", "Apple"));
   });
 
 });
